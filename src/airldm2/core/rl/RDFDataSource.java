3b5b8fe11a53d40ad483db4b5a2f315458e950bc
DongnoD <DongnoD@gmail.com>
2011-02-15 23:22:20 +0000

    + sesame 2.3.2 + virtuoso

--- src/airldm2/core/rl/RDFDataSource.java
+++ src/airldm2/core/rl/RDFDataSource.java
@@ -1,5 +1,8 @@
 package airldm2.core.rl;
 
+import org.openrdf.repository.Repository;
+
+import virtuoso.sesame2.driver.VirtuosoRepository;
 import airldm2.core.ISufficentStatistic;
 import airldm2.core.SSDataSource;
 import airldm2.exceptions.RTConfigException;
@@ -7,53 +10,46 @@ import airldm2.util.AttribValuePair;
 
 public class RDFDataSource implements SSDataSource {
 
+   private Repository mRepository;
+
    public RDFDataSource(String context) {
-      // TODO Auto-generated constructor stub
+      mRepository = new VirtuosoRepository("", "dba", "dba");
    }
 
    @Override
    public void init(String config) throws RTConfigException {
-      // TODO Auto-generated method stub
-      
    }
 
    @Override
    public ISufficentStatistic getSufficientStatistic(String s) throws Exception {
-      // TODO Auto-generated method stub
       return null;
    }
 
    @Override
    public ISufficentStatistic[] getSufficientStatistic(String[] s)
          throws Exception {
-      // TODO Auto-generated method stub
       return null;
    }
 
    @Override
    public ISufficentStatistic getSufficientStatistic(AttribValuePair nameValue)
          throws Exception {
-      // TODO Auto-generated method stub
       return null;
    }
 
    @Override
    public ISufficentStatistic getSufficientStatistic(
          AttribValuePair[] nameValues) throws Exception {
-      // TODO Auto-generated method stub
       return null;
    }
 
    @Override
    public int getNumberInstances() throws Exception {
-      // TODO Auto-generated method stub
       return 0;
    }
 
    @Override
    public void setRelationName(String relationName) {
-      // TODO Auto-generated method stub
-      
    }
 
 }
