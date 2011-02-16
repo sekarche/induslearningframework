124d0445c3c4c4ac5129ad4bd23dafe67b05b925
DongnoD@gmail.com <DongnoD@gmail.com@9aeb7340-f553-0410-8c85-3b7e5e7fdab4>
2011-02-16 18:20:58 +0000

    m RDF descriptor + empty parser

---
+++ trunk/src/airldm2/core/rl/RDFDataDescriptorParser.java
@@ -0,0 +1,12 @@
+package airldm2.core.rl;
+
+public class RDFDataDescriptorParser {
+
+   public static RDFDataDescriptor parse(String descFile) {
+      
+      
+      
+      return null;
+   }
+   
+}
